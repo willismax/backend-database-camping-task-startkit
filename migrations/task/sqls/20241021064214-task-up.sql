1b3a9ecb189cb86deaa69aa3aed6516180f8fb0d
willismax <willismax.com@gmail.com>
2024-12-10 21:28:19 +0800

    Fix Task4

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -388,6 +388,7 @@ INSERT INTO "SKILL" (name)
 VALUES ('空中瑜伽');
 DELETE FROM "SKILL"
 WHERE name = '空中瑜伽';
+-- ████████  █████   █     █ 
 --  ████████  █████   █    █   █ 
 --    █ █   ██    █  █     █   █ 
 --    █ █████ ███ ███      █████ 
